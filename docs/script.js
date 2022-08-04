19d9f8f7dc620404b98d6506f7ac893bc89cf10b
dudushy <eduardotalarico1337@gmail.com>
2022-08-04 19:25:11 -0300

    Copying text

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,16 @@
 console.log("[WhatsApp-preview] init");
+
+function test() {
+  console.log("[test] test");
+}
+
+function copyText() {
+  var input = document.getElementById("input");
+  var output = document.getElementById("output");
+
+  console.log("[copyText] input", input.value);
+  console.log("[copyText] (BEFORE) output", output.innerHTML);
+
+  output.innerHTML = input.value;
+  console.log("[copyText] (AFTER) output", output.innerHTML);
+}
