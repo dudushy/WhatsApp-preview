59bee768d67d58f2b73c6bac048f876107e24aef
dudushy <eduardotalarico1337@gmail.com>
2022-08-10 20:18:48 -0300

    Save first and last Asterisk

--- docs/script.js
+++ docs/script.js
@@ -5,12 +5,35 @@ function test() {
 }
 
 function copyText() {
+  console.clear();
   var input = document.getElementById("input");
   var output = document.getElementById("output");
 
   console.log("[copyText] input", input.value);
-  console.log("[copyText] (BEFORE) output", output.innerHTML);
+  // console.log("[copyText] (BEFORE) output", output.innerHTML);
 
   output.innerHTML = input.value;
-  console.log("[copyText] (AFTER) output", output.innerHTML);
+
+  let firstAsterisk = undefined;
+  let lastAsterisk = undefined;
+
+  console.log("[copyText] - - - - - - - - - - - - - -");
+  for (const i in input.value) {
+    const char = input.value[i];
+    console.log("[copyText] char", char);
+
+    if (char == "*") {
+      if (firstAsterisk == undefined) {
+        firstAsterisk = i;
+        console.log("[copyText] firstAsterisk", firstAsterisk);
+      } else {
+        lastAsterisk = i;
+        console.log("[copyText] lastAsterisk", lastAsterisk);
+      }
+    }
+  }
+  console.log("[copyText] - - - - - - - - - - - - - -");
+
+  // console.log("[copyText] (AFTER) output", output.innerHTML);
+  console.log("[copyText] output", output.innerHTML);
 }
